b02307be47347b237aa0748464f5a2831c23897d
Mateus2314 <mateusfuntecezar10@hotmail.com>
2022-06-27 16:57:17 -0300

    Add Header with Error

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import './App.css'
 import { uuidv4, PlusCircle } from 'Libs';
 
 import styles from 'App.module.css';
-
+import { Header } from 'components/Header';
 
 export function App() {
   
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
+
+    <div className={styles.wrapper}>
+
+    <Header />
+
+      </div>
     
   )
 }
