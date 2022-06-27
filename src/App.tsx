8437b08fceae173456f69d02ef4e937c28542e49
Mateus2314 <mateusfuntecezar10@hotmail.com>
2022-06-27 16:12:32 -0300

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import { uuidv4, PlusCircle } from 'Libs';
+
+import styles from 'App.module.css';
+
+
+export function App() {
+  
+  const [count, setCount] = useState(0)
+
+  return (
+    
+  )
+}
+
